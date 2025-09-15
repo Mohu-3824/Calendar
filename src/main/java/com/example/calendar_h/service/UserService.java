19201163c4c65617c26ac83365c52a0301b214e9
Mohu-3824 <maki.yoshimura.97@gmail.com>
2025-09-15 11:29:51 +0900

    会員詳細、編集、更新機能の追加

--- src/main/java/com/example/calendar_h/service/UserService.java
+++ src/main/java/com/example/calendar_h/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.calendar_h.entity.User;
 import com.example.calendar_h.form.SignupForm;
+import com.example.calendar_h.form.UserEditForm;
 import com.example.calendar_h.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
@@ -33,6 +34,16 @@ public class UserService {
 		return userRepository.save(user);
 	}
 	
+    @Transactional
+    public void updateUser(UserEditForm userEditForm, User user) {
+        user.setName(userEditForm.getName());
+        user.setFurigana(userEditForm.getFurigana());
+        user.setPhoneNumber(userEditForm.getPhoneNumber());
+        user.setEmail(userEditForm.getEmail());
+
+        userRepository.save(user);
+    }
+	
     // メールアドレスが登録済みかどうかをチェックする
     public boolean isEmailRegistered(String email) {
         User user = userRepository.findByEmail(email).orElse(null);
@@ -43,4 +54,16 @@ public class UserService {
     public boolean isSamePassword(String password, String passwordConfirmation) {
         return password.equals(passwordConfirmation);
     }
+    
+    // ユーザーを有効にする
+    @Transactional
+    public void enableUser(User user) {
+        user.setEnabled(true);
+        userRepository.save(user);
+    }
+    
+    // メールアドレスが変更されたかどうかをチェックする
+    public boolean isEmailChanged(UserEditForm userEditForm, User user) {
+        return !userEditForm.getEmail().equals(user.getEmail());
+    }   
 }
